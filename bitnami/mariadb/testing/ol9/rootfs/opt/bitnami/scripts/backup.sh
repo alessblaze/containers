3651c27b86a360d1e2620c4bb50cafef5eac7bc1
Albert Alesso <anishsheikh2000@icloud.com>
2025-05-07 05:43:22 +0530

    mariadb backup added

--- bitnami/mariadb/testing/ol9/rootfs/opt/bitnami/scripts/backup.sh
+++ bitnami/mariadb/testing/ol9/rootfs/opt/bitnami/scripts/backup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 BACKUP_DIR="/bitnami/backups"
 TIMESTAMP=$(date +'%Y%m%d-%H%M%S')
-BACKUP_FILE="$BACKUP_DIR/backup-${TIMESTAMP}.xbstream"
+BACKUP_FILE="$BACKUP_DIR/backup-${TIMESTAMP}.mbstream"
 
 mkdir -v /tmp/backuptmp && cd /tmp/backuptmp
 # Create backup directory if it doesn't exist
@@ -11,7 +11,7 @@ mkdir -p "$BACKUP_DIR"
 
 # Run xtrabackup stream and log to stderr
 echo "[$(date)] Starting xtrabackup stream..." >&2
-xtrabackup --backup --stream=xbstream --user="$MYSQL_BACKUP_USER" --password="$MYSQL_BACKUP_PASSWORD" 2>&1 > "$BACKUP_FILE"
+mariadb-backup --backup --stream=mbstream --user="$MARIADB_BACKUP_USER" --password="$MARIADB_BACKUP_PASSWORD" 2>&1 > "$BACKUP_FILE"
 echo "[$(date)] Backup completed: $BACKUP_FILE" >&2
 
 rm -rfv /tmp/backuptmp
